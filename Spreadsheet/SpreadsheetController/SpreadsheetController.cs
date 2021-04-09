ac63d72f7f2f29f40be6a7ec13aaf389f13e185b
u1095942@eng.utah.edu <u1095942@eng.utah.edu>
2021-04-09 11:37:01 -0600

    SpreadsheetController -Added SpreadsheetController project to mediate passing info between the GUI and the server. -Added dll libraries for TCP networking and Json objects.

---
+++ Spreadsheet/SpreadsheetController/SpreadsheetController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Controller
+{
+    /// <summary>
+    /// Mediates the passing of information between the GUI and the spreadsheet 
+    /// server. 
+    /// </summary>
+    public class SpreadsheetController
+    {
+
+    }
+}

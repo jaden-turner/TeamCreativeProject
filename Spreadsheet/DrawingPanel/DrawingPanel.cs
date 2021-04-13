28d7b77a80efeb571ee6d988a99e1c8043749a09
Trinal0821 <Trinal0821@gmail.com>
2021-04-12 21:03:25 -0600

    Worked on displaying users and added a drawing panel

---
+++ Spreadsheet/DrawingPanel/DrawingPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingPanel
+{
+    class DrawingPanel
+    {
+        public void DisplayRectangle(PaintEventArgs e)
+        {
+            Pen bluePen = new Pen(Color.Blue, 3);
+
+            Rectangle rect = new Rectangle(0, 0, 200, 200);
+
+            e.Graphics.DrawRectangle(bluePen, rect);
+
+        }
+    }
+}
--- Spreadsheet/SpreadsheetGUI/Program.cs
+++ Spreadsheet/SpreadsheetGUI/Program.cs
@@ -9,6 +9,8 @@ namespace SpreadsheetGUI
 {
     static class Program
     {
+
+        private static SpreadsheetController ssCtrl = new SpreadsheetController();
         /// <summary>
         /// Keeps track of how many top-level forms are running
         /// </summary>
@@ -56,6 +58,24 @@ namespace SpreadsheetGUI
             }
 
         }
+
+        //Handle mouse down when the user click on the cell
+        private static void HandleMouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                ssCtrl.HandleMouseRequest();
+            }
+        }
+
+        //Handle mouse up when the user click on the cell
+        private static void HandleMouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                sssCtrl.CancelMouseRequest();
+            }
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -69,38 +89,39 @@ namespace SpreadsheetGUI
             DemoApplicationContext appContext = DemoApplicationContext.getAppContext();
             //appContext.RunForm(new Spreadsheet());
 
-            // Open the connection prompt
-            ConnectionPrompt popup = new ConnectionPrompt();
-            Application.Run(popup);
+            //// Open the connection prompt
+            //ConnectionPrompt popup = new ConnectionPrompt();
+            //Application.Run(popup);
 
-            // Capture the result
-            string IPaddress = popup.getIPAddress();
-            string userName = popup.getUserName();
+            //// Capture the result
+            //string IPaddress = popup.getIPAddress();
+            //string userName = popup.getUserName();
 
-            popup.Dispose();
+            //popup.Dispose();
 
-            // Prompt window was closed
-            if (IPaddress == "")
-                return;
+            //// Prompt window was closed
+            //if (IPaddress == "")
+            //    return;
 
-            SpreadsheetController ssCtrl = new SpreadsheetController();
+           
 
-            // Prompt user input
-            SelectionPrompt selPmt = new SelectionPrompt(ssCtrl, IPaddress, userName);
-            if(!selPmt.errorOccured)
-            Application.Run(selPmt);
+            //// Prompt user input
+            //SelectionPrompt selPmt = new SelectionPrompt(ssCtrl, IPaddress, userName);
+            //if(!selPmt.errorOccured)
+            //Application.Run(selPmt);
 
-            // Capture the result
-            string ssName = selPmt.selection;
+            //// Capture the result
+            //string ssName = selPmt.selection;
 
-            selPmt.Dispose();
+            //selPmt.Dispose();
 
-            // Seleciton window was closed
-            if (selPmt.selection == "")
-                return;
+            //// Seleciton window was closed
+            //if (selPmt.selection == "")
+            //    return;
 
             // Open a spreadsheet form
-            SpreadsheetForm ssForm = new SpreadsheetForm(ssCtrl, ssName);
+            // SpreadsheetForm ssForm = new SpreadsheetForm(ssCtrl, ssName);
+            SpreadsheetForm ssForm = new SpreadsheetForm(ssCtrl, "testing");
             appContext.RunForm(ssForm);
             Application.Run(appContext);
         }
